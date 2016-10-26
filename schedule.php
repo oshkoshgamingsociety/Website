511327f5aae1f304df640a1d1b596e2a03621a82
oshkoshgamingsociety <oshkoshgamingsociety@gmail.com>
2016-10-26 18:29:52 -0500

    putting the schedule in a content tag

--- schedule.php
+++ schedule.php
@@ -4,6 +4,8 @@
 </head>
 <body>
 	<?php include "NavTemplate.php" ?>
-	<iframe src="https://calendar.google.com/calendar/embed?src=oshkoshgamingsociety%40gmail.com&ctz=America/Chicago" style="border: 0" width="800" height="600" frameborder="0" scrolling="no"></iframe>
+	<div class="container">
+		<iframe src="https://calendar.google.com/calendar/embed?src=oshkoshgamingsociety%40gmail.com&ctz=America/Chicago" style="border: 0" width="800" height="600" frameborder="0" scrolling="no"></iframe>
+	</div>
 </body>
 </html>
