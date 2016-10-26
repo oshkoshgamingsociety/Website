ae962bfc389eb2257634c026176b44ac1f825845
oshkoshgamingsociety <oshkoshgamingsociety@gmail.com>
2016-10-26 18:23:06 -0500

    creating a template file for the navigation element and inserting it into both the hello and schedule files.

--- hello.php
+++ hello.php
@@ -3,29 +3,7 @@
 	<?php include "HeadTemplate.php" ?>
 </head>
 <body>
-	 <nav class="navbar navbar-inverse navbar-fixed-top">
-      <div class="container">
-        <div class="navbar-header">
-          <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#navbar-collapse-1" aria-expanded="false" aria-controls="navbar">
-            <span class="sr-only">Toggle navigation</span>
-            <span class="icon-bar"></span>
-            <span class="icon-bar"></span>
-            <span class="icon-bar"></span>
-          </button>
-          <a class="navbar-brand" href="#"></a>
-        </div>
-        <div class="collapse navbar-collapse navbar-right" id="navbar-collapse-1">
-			<ul class="nav navbar-nav">
-				<li class="active"><a href="#">Home</a></li>
-				<li><a href="#">About</a></li>
-				<li><a href="schedule.html">Schedule</a></li>
-				<li><a href="#">History</a></li>
-				<li><a href="#">Games</a></li>
-				<li><a href="#">Links</a></li>
-			</ul>
-        </div>
-      </div>
-    </nav>
+	<?php include "NavTemplate.php" ?>
     <div class="container">
 		<h1>Oshkosh Gaming Society</h1>
 		<hr>
---
+++ schedule.php
@@ -0,0 +1,9 @@
+<html>
+<head>
+	<?php include "HeadTemplate.php" ?>
+</head>
+<body>
+	<?php include "NavTemplate.php" ?>
+	<iframe src="https://calendar.google.com/calendar/embed?src=oshkoshgamingsociety%40gmail.com&ctz=America/Chicago" style="border: 0" width="800" height="600" frameborder="0" scrolling="no"></iframe>
+</body>
+</html>
